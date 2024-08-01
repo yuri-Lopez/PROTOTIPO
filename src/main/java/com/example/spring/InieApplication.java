0b01a91185a2d14b88ba65406628c4a44a288c1e
LOPEZ YURI <yurilopezaraque@hotmail.com>
2024-07-31 20:35:22 -0500

    Version 1

---
+++ src/main/java/com/example/spring/InieApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring/InieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
